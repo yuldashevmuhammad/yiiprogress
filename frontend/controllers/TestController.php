2033aae40d67b1930fa258edc49b8260ca791cf0
Muhammadjon  Yuldashev <81702499+yuldashevmuhammad@users.noreply.github.com>
2021-04-27 16:33:50 +0500

    Test controller yaratildi va index faylga

---
+++ frontend/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
---
+++ frontend/views/test/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Bu test controllerni index fayli";
