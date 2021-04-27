a0d6dfa459454835f16f8c79b56fc2e97de3ee49
Muhammadjon  Yuldashev <81702499+yuldashevmuhammad@users.noreply.github.com>
2021-04-27 16:57:30 +0500

    post controllerni index fayliga forma qo0shildi

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -1,4 +1,14 @@
 <?php
 
 echo "Bu post controllerni index fayli";
-echo "uchunchi marta ozgartirish kiritdim";
+echo "uchunchi marta ozgartirish kiritdim";?>
+
+<form>
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username"><br>
+
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password"><br><br>
+
+    <button type="submit">Yuborish</button>
+</form>
