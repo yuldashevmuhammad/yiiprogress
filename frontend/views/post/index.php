7c23b7e2b77929fd5079c526e25914231a2f0783
Muhammadjon  Yuldashev <81702499+yuldashevmuhammad@users.noreply.github.com>
2021-04-27 17:01:55 +0500

    formani stili ozgardi

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -3,12 +3,13 @@
 echo "Bu post controllerni index fayli";
 echo "uchunchi marta ozgartirish kiritdim";?>
 
-<form>
-    <label for="username">Username</label>
+<form class="form-group-lg">
+    <label for="username" class="label label-primary">Username</label>
     <input type="text" name="username" id="username"><br>
 
-    <label for="password">Password</label>
+    <label for="password" class="label label-primary
+">Password</label>
     <input type="password" name="password" id="password"><br><br>
 
-    <button type="submit">Yuborish</button>
+    <button type="submit" class="btn btn-success">Yuborish</button>
 </form>
