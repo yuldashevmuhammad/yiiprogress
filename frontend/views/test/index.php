daee8f0a5b72a56e730049da15b5ce5afb7f0c94
Muhammadjon  Yuldashev <81702499+yuldashevmuhammad@users.noreply.github.com>
2021-04-28 09:07:04 +0500

    yangi forma qoshildi

--- frontend/views/test/index.php
+++ frontend/views/test/index.php
@@ -1,4 +1,19 @@
 <?php
 
 echo "Bu test controllerni index fayli";
-echo "Test controller index fayliga yangi echo yozildi";
+echo "Test controller index fayliga yangi echo yozildi"; ?>
+
+<form action="" method="post">
+
+    <label for="name">ismi</label>
+    <input type="text" name="ism"> <br>
+
+    <label for="familiyasi">Familiyasi</label>
+    <input type="text" name="fam" id="familiasyi">
+
+    <button type="submit"> Yuborish</button>
+
+
+
+
+</form>
